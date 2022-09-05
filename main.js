8a0098fdc8acad3c3b1631d7b9a7b4b5d77fdf25
thomasrcham <guseldorph@gmail.com>
2022-09-05 13:22:16 -0500

    done it

--- main.js
+++ main.js
@@ -4,22 +4,64 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+let likeButton = document.getElementsByClassName('like')
+let heart = document.querySelectorAll('li span')
 
+for (let i = 0; i < likeButton.length; i++){
+  likeButton[i].addEventListener('click', (e) => {
+    fetchAnswer(i);
+    })
+}
 
+function changeHeart (heartNumber) {
+  console.log('change')
+  if (heart[heartNumber].hasAttribute('flag')) {
+    heart[heartNumber].setAttribute('class','like-glyph');
+    heart[heartNumber].removeAttribute('flag')
+  } else {
+      heart[heartNumber].setAttribute('class','activated-heart');
+      heart[heartNumber].setAttribute('flag','1');
+    }
+}
 
+function fetchAnswer (heartNumber) {
+  // console.log(`clicked ${heartNumber}`)
+  mimicServerCall()
+    .then(res => {
+      changeHeart(heartNumber)})
+    .catch(err => {
+      errorMessageOn();
+      setTimeout(errorMessageOff, 3000);
+      console.log('fail')})
+}
+
+function checkResponse (res) {
+  if (res === true) {
+    console.log('yay')
+  } else {console.log ('booo')}
+}
+
+function errorMessageOn (){
+  document.querySelector('body div').setAttribute('class', 'visible')
+}
+
+function errorMessageOff (){
+  document.querySelector('body div').setAttribute('class', 'hidden')
+}
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
 
 function mimicServerCall(url="http://mimicServer.example.com", config={}) {
-  return new Promise(function(resolve, reject) {
+return new Promise(function(resolve, reject) {
     setTimeout(function() {
       let isRandomFailure = Math.random() < .2
       if (isRandomFailure) {
         reject("Random server error. Try again.");
       } else {
-        resolve("Pretend remote server notified of action!");
+        resolve();
       }
     }, 300);
   });
+  
 }
